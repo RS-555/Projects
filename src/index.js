159cb53ae543a5076de0e0b384ddfc9632bac2da
RS-555 <rishabh.work68@gmail.com>
2021-01-21 21:41:08 +0530

    file modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
+import "./style.css";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+function Lang(Props) {
+  return (
+    <div className="lang">
+      <h1>{Props.head}</h1>
+      <ul>
+        <li>{Props.one}</li>
+        <li>{Props.two}</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+var app = (
+  <div>
+    <Lang head="Languages" one="React Native" two="React JS" />
+    <Lang head="BackEnd Languages" one="Node" two="Express" />
+  </div>
+);
+
+ReactDOM.render(app, document.querySelector("#app"));
